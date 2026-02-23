5569e875f52ab3de846244e6b2fd9c6ea3702242
pari9p <behlpari4@gmail.com>
2026-02-23 16:59:08 +0530

    Implement logging setup utility

---
+++ llmchain/utils/logging_setup.py
@@ -0,0 +1,9 @@
+import logging
+
+def setup_logging(level=logging.INFO):
+    logging.basicConfig(
+        level=level,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info("Logging is set up.")
